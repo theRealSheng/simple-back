020ca84747531592cc0defd1756eb5900710cf71
theRealSheng <sheng.huchai@gmail.com>
2018-03-01 19:42:40 +0100

    Add back route to create warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -17,4 +17,19 @@ router.get('/:id', (req, res, next) => {
     .catch(next);
 });
 
+router.post('/', (req, res, next) => {
+  const event = req.body;
+  const newWarehouse = Warehouse({
+    ownerID: event.ownerID,
+    companyName: event.companyName,
+    warehouseAddress: event.warehouseAddress,
+    pricing: event.pricing,
+    specialNotes: event.specialNotes,
+    description: event.description
+  });
+
+  return newWarehouse.save()
+    .then(() => res.json(newWarehouse));
+});
+
 module.exports = router;
