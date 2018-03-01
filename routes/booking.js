a4cad50c3e948c62a4b9b3a19075c2d8f5a204dd
theRealSheng <sheng.huchai@gmail.com>
2018-03-01 16:54:19 +0100

    Adjust booking route

--- routes/booking.js
+++ routes/booking.js
@@ -11,7 +11,6 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const event = req.body;
-
   const newBooking = Booking({
     warehouseAddress: event.warehouseAddress,
     seller: event.seller,
